35a746b57f9205828a1a9b893c35897d9236c012
Andrey Bezrukov <andruxa.ic@gmail.com>
2021-08-07 12:15:25 +0400

    Реализована отправка личных сообщений

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -37,18 +37,46 @@ public class Server {
         }
     }
 
-    public void broadcastMsg(ClientHandler sender, String msg){
+    public void broadcastMsg(ClientHandler sender, String msg) {
         String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
         for (ClientHandler c : clients) {
             c.sendMsg(message);
         }
     }
 
-    public void subscribe(ClientHandler clientHandler){
+    public void sendWhisper(ClientHandler sender, String recipientNick, String msg) {
+        ClientHandler recipient = getClientByNickname(recipientNick);
+
+        if (recipient != null) {
+            if (recipient == sender) {
+                sender.sendMsg("Разговариваем сами с собой?");
+            } else {
+
+                String messageFrom = String.format("Личное сообщение для %s: %s", recipientNick, msg);
+                String messageTo = String.format("Личное сообщение от %s: %s", sender.getNickname(), msg);
+
+                sender.sendMsg(messageFrom);
+                recipient.sendMsg(messageTo);
+            }
+        } else {
+            sender.sendMsg(String.format("Пользователя %s нет в чате", recipientNick));
+        }
+    }
+
+    private ClientHandler getClientByNickname(String nick) {
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(nick)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
         clients.add(clientHandler);
     }
 
-    public void unsubscribe(ClientHandler clientHandler){
+    public void unsubscribe(ClientHandler clientHandler) {
         clients.remove(clientHandler);
     }
 
